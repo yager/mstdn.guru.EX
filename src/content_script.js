08988a6bba5b7b4cc6441f78c39e597058b8d803
7Ymkvvmi9C4xXUd <yager@creazy.net>
2020-09-09 15:50:37 +0900

    ver1.2

--- src/content_script.js
+++ src/content_script.js
@@ -6,7 +6,6 @@ function main(e) {
         if (document.querySelector('#mastodon') != null) {
             renderHTML();
             clearInterval(jsInitCheckTimer);
-  //要素を取得する処理
         }
     }
 }
@@ -14,11 +13,11 @@ function main(e) {
 function renderHTML() {
     if ( document.querySelector('.drawer__inner__mastodon') != null ) {
         document.querySelector('.drawer__inner__mastodon').innerHTML
-            = '<iframe src="https://mixlr.com/users/3494368/embed" width="100%" height="180px" scrolling="no" frameborder="no" marginheight="0" marginwidth="0"></iframe>';
+            = '<iframe src="https://mixlr.com/users/3494368/embed" width="100%" height="150px" scrolling="no" frameborder="no" marginheight="0" marginwidth="0"></iframe>';
     }
-    if ( document.querySelector('.getting-started__footer') != null ) {
+    else if ( document.querySelector('.getting-started__footer') != null ) {
         document.querySelector('.getting-started__footer p').innerHTML
-            = '<iframe src="https://mixlr.com/users/3494368/embed" width="100%" height="180px" scrolling="no" frameborder="no" marginheight="0" marginwidth="0"></iframe>';
+            = '<iframe src="https://mixlr.com/users/3494368/embed" width="100%" height="150px" scrolling="no" frameborder="no" marginheight="0" marginwidth="0"></iframe>';
     }
 
     
