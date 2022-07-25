78ac3d03f25a551c40ba718827f4aad321b29f50
Takahito Yagami <yager@yager-mac-mini.local>
2022-07-25 13:40:34 +0900

    ver0.5

--- src/content_script.js
+++ src/content_script.js
@@ -1,4 +1,4 @@
-﻿window.addEventListener("load", main, false);
+window.addEventListener("load", main, false);
 
 function main(e) {
     const jsInitCheckTimer = setInterval(jsLoaded, 1000);
@@ -15,13 +15,13 @@ function renderHTML() {
     if ( document.querySelector('.drawer__inner__mastodon') != null ) {
         document.querySelector('.drawer__inner__mastodon').innerHTML
             = '<iframe id="ytIF" width="285" height="160" src="https://www.youtube.com/embed/?list=UUTfta7Ult6yLu7ru-WInOGg" frameborder="0" allowfullscreen></iframe>'
-            + '<iframe id="mixlrIF" src="https://mixlr.com/users/3494368/embed" width="100%" height="150px" scrolling="no" frameborder="no" marginheight="0" marginwidth="0"></iframe>';
+            + '<iframe src="https://player.twitch.tv/?channel=backspacefm&parent=mstdn.guru" frameborder="0" allowfullscreen="true" scrolling="no" height="150" width="100%"></iframe>';
     }
     // 通常 UI
     else if ( document.querySelector('.getting-started__footer') != null ) {
         document.querySelector('.getting-started__footer p').innerHTML
             = '<iframe id="ytIF" width="265" height="149" src="https://www.youtube.com/embed/?list=UUTfta7Ult6yLu7ru-WInOGg" frameborder="0" allowfullscreen></iframe>'
-            + '<iframe id="mixlrIF" src="https://mixlr.com/users/3494368/embed" width="100%" height="150px" scrolling="no" frameborder="no" marginheight="0" marginwidth="0"></iframe>';
+            + '<iframe src="https://player.twitch.tv/?channel=backspacefm&parent=mstdn.guru" frameborder="0" allowfullscreen="true" scrolling="no" height="150" width="100%"></iframe>';
     }
 
     
